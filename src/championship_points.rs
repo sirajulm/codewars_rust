efc536c25de97c7500dd2a3e015316a769c2b68b
Sirajul Muneer <siraj.mussafirr@gmail.com>
2023-04-14 21:28:58 +0200

    [kata] Championship point for a team

---
+++ src/championship_points.rs
@@ -0,0 +1,36 @@
+use std::cmp::Ordering;
+
+pub fn championship_points(games: &[String]) -> u32 {
+  games.iter().map(|s| {
+    let (l,r) = s.split_once(':').unwrap();
+    match l.cmp(r) {
+      Ordering::Less => 0,
+      Ordering::Equal => 1,
+      Ordering::Greater => 3,
+    }
+  }).sum()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::championship_points;
+    
+    const ERR_MSG: &str = "\nYour result (left) did not match the expected output (right)";
+    
+    fn do_fixed_test(e: &[&str], expected: u32) {
+        let a = &e.iter().map(|x| x.to_string()).collect::<Vec<_>>();
+        assert_eq!(championship_points(a), expected, "{ERR_MSG} with games = {a:?}")
+    }
+
+    #[test]
+    fn fixed_tests() {
+        do_fixed_test(&["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"], 30);
+        do_fixed_test(&["1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4"], 10);
+        do_fixed_test(&["0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4"], 0);
+        do_fixed_test(&["1:0", "2:0", "3:0", "4:0", "2:1", "1:3", "1:4", "2:3", "2:4", "3:4"], 15);
+        do_fixed_test(&["1:0", "2:0", "3:0", "4:4", "2:2", "3:3", "1:4", "2:3", "2:4", "3:4"], 12);
+    }
+}
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 pub mod boolean_to_string;
 pub mod boolean_to_word;
-pub mod dna_to_rna;
+pub mod championship_points;
 pub mod clock_past;
+pub mod cockroach_speed;
+pub mod dna_to_rna;
 pub mod double_integer;
 pub mod third_angle_of_triangle;
-pub mod cockroach_speed;
